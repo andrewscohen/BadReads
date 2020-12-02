c6e0a58df2d0af0b977795f750adec799f6402f2
River <cosmos.cha1997@gmail.com>
2020-12-02 18:03:05 -0500

    added to pug pages for home and layout (taskbar)

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const csrf = require("csrf");
+const { decodeBase64 } = require("bcryptjs");
 const router = express.Router();
 
 const csrfProtection = csrf({ cookie: true });
 const asyncHandler = (handler) => (req, res, next) =>
   handler(req, res, next).catch(next);
 
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "a/A Express Skeleton Home" });
-});
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const books = await db.Book.findAll();
+    res.render("home", { title: "badReads", books });
+  })
+);
 
 module.exports = {
   express,
