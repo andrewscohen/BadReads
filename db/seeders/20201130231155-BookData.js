faed9a640b97ad87a7159c566e50d7da75c9f797
River <cosmos.cha1997@gmail.com>
2020-12-02 12:28:02 -0500

    made publishers and added Twilight books

--- db/seeders/20201130231155-BookData.js
+++ db/seeders/20201130231155-BookData.js
@@ -1,17 +1,77 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Books', [
-      { name: '', description: '', pageCount: '', publisherId: '', createdAt: new Date(), updatedAt: new Date() },
-      { name: '', description: '', pageCount: '', publisherId: '', createdAt: new Date(), updatedAt: new Date() },
-      { name: '', description: '', pageCount: '', publisherId: '', createdAt: new Date(), updatedAt: new Date() },
-      { name: '', description: '', pageCount: '', publisherId: '', createdAt: new Date(), updatedAt: new Date() },
-      { name: '', description: '', pageCount: '', publisherId: '', createdAt: new Date(), updatedAt: new Date() }
+    return queryInterface.bulkInsert("Books", [
+      {
+        name: "Twilight",
+        description:
+          "Twilight is about a young girl getting gaslit into believing her life is worth throwing away for a shimmery vampire.",
+        pageCount: "501",
+        publisherId: "3",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "New Moon",
+        description: "the 2nd Twilight book",
+        pageCount: "563",
+        publisherId: "3",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Eclipse",
+        description: "the 3nd Twilight book",
+        pageCount: "629",
+        publisherId: "3",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Breaking Dawn",
+        description: "the 4th Twilight book",
+        pageCount: "756",
+        publisherId: "3",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Midnight Sun",
+        description: "the 5th Twilight book",
+        pageCount: "662",
+        publisherId: "3",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Fifty Shades of Grey",
+        description: "",
+        pageCount: "",
+        publisherId: "",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "",
+        description: "",
+        pageCount: "",
+        publisherId: "",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "",
+        description: "",
+        pageCount: "",
+        publisherId: "",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Books', null, {});
-  }
+    return queryInterface.bulkDelete("Books", null, {});
+  },
 };
--- db/seeders/20201130231155-PublisherData.js
+++ db/seeders/20201130231155-PublisherData.js
@@ -1,17 +1,42 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Publishers', [
-      { name: '', location: '', createdAt: new Date(), updatedAt: new Date() },
-      { name: '', location: '', createdAt: new Date(), updatedAt: new Date() },
-      { name: '', location: '', createdAt: new Date(), updatedAt: new Date() },
-      { name: '', location: '', createdAt: new Date(), updatedAt: new Date() },
-      { name: '', location: '', createdAt: new Date(), updatedAt: new Date() }
+    return queryInterface.bulkInsert("Publishers", [
+      {
+        name: "Scholastic Incorporated",
+        location: "New York, New York",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Penguin Random House LLC",
+        location: "New York, New York",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Little,Brown & Company",
+        location: "Boston, Massachusetts",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Random House Vintage Books",
+        location: "New York, New York",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Nosy Crow",
+        location: "London, United Kingdom",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Publishers', null, {});
-  }
+    return queryInterface.bulkDelete("Publishers", null, {});
+  },
 };
