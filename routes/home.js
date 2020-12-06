ac0033e7a541e988dbe57b94d34f438c9ee16701
ramses <ramses.romero.jr@gmail.com>
2020-12-06 00:14:18 -0500

    got the genre filter working and displaying what genre is displaying

--- routes/home.js
+++ routes/home.js
@@ -6,11 +6,31 @@ const router = express.Router();
 router.get(
   "/",
   asyncHandler(async (req, res) => {
-    const books = await db.Book.findAll();
     const genres = await db.Genre.findAll();
+    const books = await db.Book.findAll();
     res.render("home", { title: "Bad Reads", books, genres });
+    // res.json(genres)
+
   })
 );
 
+router.get('/genreFilter', asyncHandler(async(req,res)=>{
+  const {genreId} = req.query
+  const genres = await db.Genre.findAll();
 
+  const books = await db.Book.findAll({
+    include:[{
+      model:db.Genre,
+      where: {id: genreId},
+      through: {attributes:['genreId']}
+    }]
+  })
+  const genre = await db.Genre.findByPk(genreId)
+  const genreName = genre.name
+
+  res.render("home", { title: "Bad Reads", books, genres, genreName });
+  // res.json({genreName})
+
+
+}))
 module.exports = router;
